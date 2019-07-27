bf54efe98c1e354bf79565db8fd7348e7115d4cd
Aslan Varoqua (Skylines Digital USA) <aslan.varoqua@gmail.com>
2019-07-27 07:30:57 +0000

    Update cutline.sh

--- cutline.sh
+++ cutline.sh
@@ -4,7 +4,7 @@
 
 # NASA_RAW="/home/ubuntu/geoserver_data/workspacces/latest/nasa_in/"
 #TIFS="/home/ubuntu/geoserver_data/workspaces/latest/tif_in"
-UUIDS="/home/ubuntu/geoserver_data/workspaces/latest/shp_in/"
+UUIDS="/home/ubuntu/geoservelakes/workspaces/master/lakes/*"
 # SHP_MASTER="/home/ubuntu/geoserver_data/workspaces/master/lakes/geometry.shp"
 # for file in $NASA_RAW; do  saga_cmd io_gdal 0 -FILES=file -SELECT -SELECT_SORT -TRANSFORM -RESAMPLING=3 ; done
 # saga_cmd io_gdal 3 -FILES="/home/ubuntu/geoserver_data/workspaces/master/geometry.shp" -GEOM_TYPE=0
@@ -17,7 +17,7 @@ threads=$(( $threads - 1))
 for UUID in $UUIDS:
 do
     CUTLINE=/home/ubuntu/geoservelakes/workspaces/master/lakes/$UUID.shp
-    GEOTIFF_IN=/home/ubuntu/Downloads/2019-07-27/sgrd/Clipped.tif
+    GEOTIFF_IN=/home/ubuntu/Downloads/Clipped.tif
     GEOTIFF_OUT=/home/ubuntu/Downloads/2019-07-27/tif/$UUID.TIF
     COUNTOUR_OUT=/home/ubuntu/Downloads/2019-07-27/tif/$UUID.shp
 
@@ -47,10 +47,10 @@ do
   for thread in `seq $threads`
   
   do
-    CUTLINE=/home/ubuntu/geoserver_data/workspaces/latest/shp_in/$UUID.shp
-    GEOTIFF_IN=/home/ubuntu/geoserver_data/workspaces/latest/tif_in/latest.tif
-    GEOTIFF_OUT=/home/ubuntu/geoserver_data/workspaces/latest/tif_out/$UUID.TIF
-    COUNTOUR_OUT=/home/ubuntu/geoserver_data/workspaces/latest/countour_out/$UUID.TIF
+    CUTLINE=/home/ubuntu/geoservelakes/workspaces/master/lakes/$UUID.shp
+    GEOTIFF_IN=/home/ubuntu/Downloads/Clipped.tif
+    GEOTIFF_OUT=/home/ubuntu/Downloads/2019-07-27/tif/$UUID.TIF
+    COUNTOUR_OUT=/home/ubuntu/Downloads/2019-07-27/tif/$UUID.shp
 
     gdalwarp \
       -s_srs EPSG:4326 \
