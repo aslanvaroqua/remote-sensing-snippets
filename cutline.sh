2aba5eea48bbddb3df36ba9f347a640f4b258378
Aslan Varoqua (Skylines Digital USA) <aslan.varoqua@gmail.com>
2019-07-27 08:14:12 -0500

    Update cutline.sh

--- cutline.sh
+++ cutline.sh
@@ -10,73 +10,40 @@ UUIDS="/home/ubuntu/geoservelakes/workspaces/master/lakes/*"
 # saga_cmd io_gdal 3 -FILES="/home/ubuntu/geoserver_data/workspaces/master/geometry.shp" -GEOM_TYPE=0
 # saga_cmd grid_tools 3 -GRIDS=$TIFS -NAME=Mosaic -TYPE=7 -RESAMPLING=3 -OVERLAP=1 -MATCH=0 -TARGET_DEFINITION=0 -TARGET_USER_SIZE=0.000630 -TARGET_USER_XMIN=-126.351710 -TARGET_USER_XMAX=-70.101882 -TARGET_USER_YMIN=26.015462 -TARGET_USER_YMAX=48.655960 -TARGET_USER_FITS=0
 # saga_cmd shapes_grid 7 -INPUT=$TIFS -POLYGONS=$SHP_MASTER
+def proccess_uuid(UUID):
+        CUTLINE="/home/ubuntu/geoservelakes/workspaces/master/lakes/" + UUID + ".shp"
+        GEOTIFF_IN="/home/ubuntu/Downloads/Clipped.tif"
+        GEOTIFF_OUT="/home/ubuntu/Downloads/2019-07-27/tif/" + UUID + ".TIF"
+        COUNTOUR_OUT="/home/ubuntu/Downloads/2019-07-27/tif/" + UUID + ".shp"
+        os.system("gdalwarp   -s_srs EPSG:4326 -t_srs EPSG:4326  -of GTiff  -cutline "+ CUTLINE + " -crop_to_cutline  -dstalpha  " + GEOTIFF_IN   + " " +    GEOTIFF_OUT)
+        os.system("gdal_edit.py       -a_srs EPSG:4326 " +       GEOTIFF_OUT)
+        os.system('gdal_contour -b 1 -a ECO_LST_F -i 3.0 -f "ESRI Shapefile" ' + GEOTIFF_OUT + " " + COUNTOUR_OUT)
+        print( "done processing " +  UUID)
+        return
+        
+ 
+        
+def worker():
+    files= os.listdir("/home/ubuntu/geoservelakes/workspaces/master/lakes/")
+    files.reverse()
+    for file in files:
+        if "shp" in file:
+            process=True
+            UUID= file.replace(".shp","")
+            for f in os.listdir("/home/ubuntu/Downloads/2019-07-27/tif/"):
+                    if "shp" in f and UUID in f:
+                        print("already processed")
+                        process = False
+            if process:
+                print("new-process")
+                proccess_uuid(UUID)
 
-threads=30;
+threads = []
 
-threads=$(( $threads - 1))
-for UUID in $UUIDS:
-do
-    CUTLINE=/home/ubuntu/geoservelakes/workspaces/master/lakes/$UUID.shp
-    GEOTIFF_IN=/home/ubuntu/Downloads/Clipped.tif
-    GEOTIFF_OUT=/home/ubuntu/Downloads/2019-07-27/tif/$UUID.TIF
-    COUNTOUR_OUT=/home/ubuntu/Downloads/2019-07-27/tif/$UUID.shp
-
-    gdalwarp \
-      -s_srs EPSG:4326 \
-      -t_srs EPSG:4326 \
-      -of GTiff \
-      -cutline $CUTLINE \
-      -crop_to_cutline \
-      -dstalpha \
-      $GEOTIFF_IN \
-      $GEOTIFF_OUT
-
-    gdal_edit.py \
-      -a_srs EPSG:4326 \
-      $GEOTIFF_OUT
-
-    gdal_contour \
-      -b 1 \
-      -a ECO_LST_F \
-      -i 3.0 \
-      -f "ESRI Shapefile" \
-      $GEOTIFF_OUT \
-      $COUNTOUR_OUT
-
-     echo "$UUID" complete
-  for thread in `seq $threads`
-  
-  do
-    CUTLINE=/home/ubuntu/geoservelakes/workspaces/master/lakes/$UUID.shp
-    GEOTIFF_IN=/home/ubuntu/Downloads/Clipped.tif
-    GEOTIFF_OUT=/home/ubuntu/Downloads/2019-07-27/tif/$UUID.TIF
-    COUNTOUR_OUT=/home/ubuntu/Downloads/2019-07-27/tif/$UUID.shp
-
-    gdalwarp \
-      -s_srs EPSG:4326 \
-      -t_srs EPSG:4326 \
-      -of GTiff \
-      -cutline $CUTLINE \
-      -crop_to_cutline \
-      -dstalpha \
-      $GEOTIFF_IN \
-      $GEOTIFF_OUT
-
-    gdal_edit.py \
-      -a_srs EPSG:4326 \
-      $GEOTIFF_OUT
-
-    gdal_contour \
-      -b 1 \
-      -a ECO_LST_F \
-      -i 3.0 \
-      -f "ESRI Shapefile" \
-      $GEOTIFF_OUT \
-      $COUNTOUR_OUT
-
-     echo "$UUID" complete
-   done
-done < "$input"
+for i in range(22):
+    t = threading.Thread(target=worker)
+    threads.append(t)
+    t.start()
 
 #python
 # for x in os.listdir("/home/ubuntu/geoserver_data/workspaces/tif_in/"):
