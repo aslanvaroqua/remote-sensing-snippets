ffc7ba93dec36085fe5110dce09dbd2a8967a4ee
Aslan Varoqua (Skylines Digital USA) <aslan.varoqua@gmail.com>
2019-07-26 19:38:03 -0500

    Update cutline.sh

--- cutline.sh
+++ cutline.sh
@@ -2,14 +2,14 @@
 # export SAGA_MLB=/usr/lib/saga
 
 
-NASA_RAW="/home/ubuntu/geoserver_data/workspacces/latest/nasa_in/"
-TIFS="/home/ubuntu/geoserver_data/workspaces/latest/tif_in"
-UUIDS="/home/ubuntu/geoserver_data/workspaces/latest/shp_in/"
-SHP_MASTER="/home/ubuntu/geoserver_data/workspaces/master/lakes/geometry.shp"
-for file in $NASA_RAW; do  saga_cmd io_gdal 0 -FILES=file -SELECT -SELECT_SORT -TRANSFORM -RESAMPLING=3 ; done
-saga_cmd io_gdal 3 -FILES="/home/ubuntu/geoserver_data/workspaces/master/geometry.shp" -GEOM_TYPE=0
-saga_cmd grid_tools 3 -GRIDS=$TIFS -NAME=Mosaic -TYPE=7 -RESAMPLING=3 -OVERLAP=1 -MATCH=0 -TARGET_DEFINITION=0 -TARGET_USER_SIZE=0.000630 -TARGET_USER_XMIN=-126.351710 -TARGET_USER_XMAX=-70.101882 -TARGET_USER_YMIN=26.015462 -TARGET_USER_YMAX=48.655960 -TARGET_USER_FITS=0
-saga_cmd shapes_grid 7 -INPUT=$TIFS -POLYGONS=$SHP_MASTER
+# NASA_RAW="/home/ubuntu/geoserver_data/workspacces/latest/nasa_in/"
+# TIFS="/home/ubuntu/geoserver_data/workspaces/latest/tif_in"
+# UUIDS="/home/ubuntu/geoserver_data/workspaces/latest/shp_in/"
+# SHP_MASTER="/home/ubuntu/geoserver_data/workspaces/master/lakes/geometry.shp"
+# for file in $NASA_RAW; do  saga_cmd io_gdal 0 -FILES=file -SELECT -SELECT_SORT -TRANSFORM -RESAMPLING=3 ; done
+# saga_cmd io_gdal 3 -FILES="/home/ubuntu/geoserver_data/workspaces/master/geometry.shp" -GEOM_TYPE=0
+# saga_cmd grid_tools 3 -GRIDS=$TIFS -NAME=Mosaic -TYPE=7 -RESAMPLING=3 -OVERLAP=1 -MATCH=0 -TARGET_DEFINITION=0 -TARGET_USER_SIZE=0.000630 -TARGET_USER_XMIN=-126.351710 -TARGET_USER_XMAX=-70.101882 -TARGET_USER_YMIN=26.015462 -TARGET_USER_YMAX=48.655960 -TARGET_USER_FITS=0
+# saga_cmd shapes_grid 7 -INPUT=$TIFS -POLYGONS=$SHP_MASTER
 
 threads=30;
 
